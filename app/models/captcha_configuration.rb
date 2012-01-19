cb66dda37eae145c5df6f0e4eaaae5baa13c54be
Satish Bhat <bhattisatish@gmail.com>
2012-01-19 15:12:24 +0530

    Corrected the admin configuration path and default to environment variable

--- app/models/captcha_configuration.rb
+++ app/models/captcha_configuration.rb
@@ -2,8 +2,8 @@ class CaptchaConfiguration < Configuration
   THEMES = ['red', 'white', 'blackglass', 'clean', 'custom']
 
   # this keys works for localhost
-  preference :private_key, :string, :default => '6LfIsgEAAAAAAGfB6Z0lEUtdL3GVuBkRa9cYlMZz'
-  preference :public_key, :string, :default => '6LfIsgEAAAAAALpT20eiC3RslZQmmCbiNS-AUvSe'
+  preference :private_key, :string, :default => ENV['RECAPTCHA_PUBLIC_KEY'] || '6LfIsgEAAAAAAGfB6Z0lEUtdL3GVuBkRa9cYlMZz'
+  preference :public_key, :string, :default => ENV['RECAPTCHA_PRIVATE_KEY'] || '6LfIsgEAAAAAALpT20eiC3RslZQmmCbiNS-AUvSe'
   preference :theme, :string, :default => 'red'
   preference :use_captcha, :boolean, :default => true
 end
